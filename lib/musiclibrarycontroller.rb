0ee19bebcbaa3885f875cd8d460e8c69097a3f75
Ryan Manchester <ryanm17@gmail.com>
2018-02-27 18:29:06 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -62,9 +62,10 @@ class MusicLibraryController
 
    def play_song
      puts "Which song number would you like to play?"
-     input = gets.strip
-     binding.pry
-     Song.find_by_name(input)
+     input = gets.strip.to_i
+     song = Song.all[input]
+     puts "Playing #{song.name} by #{song.artist.name}"
+
 
    end
 
